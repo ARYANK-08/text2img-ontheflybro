f17adbed4dcf2aba3c2560415781c37cf3552305
ARYANK-08 <kyathamaryan@gmail.com>
2024-11-05 23:15:21 +0530

    improved quality

--- api/index.py
+++ api/index.py
@@ -50,9 +50,9 @@ def generate_image():
         response = client.images.generate(
             prompt=prompt,
             model="black-forest-labs/FLUX.1-schnell-Free",
-            width=1024,
-            height=768,
-            steps=1,
+            width=1280,  
+            height=768,  
+            steps=4, 
             n=1,
             response_format="b64_json"
         )
